5b79947ffc65231847057c0d580c4926e0d28ced
Frankincense <frankincensewelsey@gmail.com>
2025-04-16 20:22:56 +0300

    learn unit test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,10 @@
 // Your tests here
+import {pointsForWord} from "../utils"
+
+describe("pointsForWord", ()=>{
+    test("calculates the total points for a waord (1 point per vowel, 2 per consonant)",()=>{
+        const word = "test"
+        const result = pointsForWord(word);
+        expect(result).toBe(7);
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,16 @@
 // Your code here
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+export function pointsForWord(word){
+    let points = 0;
+    for(let letter of word){
+        if (vowels.includes(letter)){
+            points += 1;
+        }
+        else{
+            points += 2;
+        }
+    }
+    return points
+}
+
+// Side effect and data Fetching
